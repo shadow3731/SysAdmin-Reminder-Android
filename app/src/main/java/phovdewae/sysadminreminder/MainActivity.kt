e39bee653479ff3b92d1cc98c02f62b3335ac2b2
Vladislav <v1rus3731@gmail.com>
2023-05-09 15:59:48 +0300

    commit changes

---
+++ app/src/main/java/phovdewae/sysadminreminder/MainActivity.kt
@@ -0,0 +1,19 @@
+package phovdewae.sysadminreminder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import phovdewae.sysadminreminder.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bnvMain.setOnItemReselectedListener {
+
+        }
+    }
+}
